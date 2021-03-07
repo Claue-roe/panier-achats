748e38d3dfe79b1069de91a94be72db54481180a
Claudia Patry <71995934+Claue-roe@users.noreply.github.com>
2021-03-06 20:54:43 -0500

    Ajout imcomplet d'intégration avec firebase

--- src/Entete.jsx
+++ src/Entete.jsx
@@ -3,7 +3,7 @@ import ShoppingCartIcon from '@material-ui/icons/ShoppingCart';
 import Badge from '@material-ui/core/Badge';
 import SommairePanier from './SommairePanier';
 import { useState } from 'react';
-import { Link } from 'react-router-dom';
+import { Link, NavLink } from 'react-router-dom';
 
 export default function Entete(props) {
   // const etatCacherSommairePanier = useState(true);
@@ -50,9 +50,9 @@ export default function Entete(props) {
     <header className="Entete">
       <div><Link to="/">Logo</Link></div>
       <ul className="navPrincipale">
-        <li><Link to="/nos-produits">Produits</Link></li>
-        <li><Link to="/a-propos-de-magasin">À propos de nous</Link></li>
-        <li><Link to="/contact">Contactez-nous</Link></li>
+        <li><NavLink to="/nos-produits" activeClassName="navActive">Produits</NavLink></li>
+        <li><NavLink to="/a-propos-de-magasin" activeClassName="navActive">À propos de nous</NavLink></li>
+        <li><NavLink to="/contact" activeClassName="navActive">Contactez-nous</NavLink></li>
       </ul>
       <ul className="navUtil">
         <li>
--- src/ListeProduits.jsx
+++ src/ListeProduits.jsx
@@ -1,6 +1,31 @@
 import "./ListeProduits.scss";
 import Produit from "./Produit";
 import tabProduits from "./data/produits.json";
+import instanceFirebase from 'firebase/app';
+import "firebase/firestore";
+
+//Code pour intégrer Firebase temporairement placé ici
+//Configuration du projet FB
+const firebaseConfig = {
+  apiKey: "AIzaSyCALR26mfBqim8lJw9DongwsXO3agomG7Y",
+  authDomain: "panier-achats-cpatry.firebaseapp.com",
+  projectId: "panier-achats-cpatry",
+  storageBucket: "panier-achats-cpatry.appspot.com",
+  messagingSenderId: "834323949561",
+  appId: "1:834323949561:web:c4a89652abde78b46dff28",
+  measurementId: "G-N0T5DY59QR"
+};
+
+//Obtenir une instance de l'API Firebase
+if(instanceFirebase.apps.length){
+  instanceFirebase.initializeApp(firebaseConfig);
+}
+
+//Obtenir une instance de la BD Firestore
+const bd = instanceFirebase.firestore;
+
+//AJAX et dponc Promise et donc Asynx
+// let mesProduits = bd.collection("produits").get();
 
 function ListeProduits(props) {
   //console.log("Panier dans ListeProduits", props.etatPanier);
